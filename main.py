5be9174fd7c91c23fdd3df8361f680922fdf981d
Your Name <you@example.com>
2024-12-11 15:29:02 +0200

    redakt file

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ app = FastAPI(
 @app.get('/')
 def index():
     return {'status': 200}
+
+@app.get('/toor')
+def toor_int(book_id: int):
+    return {'{toor_id}': book_id}
+
+
+@app.get('/toor/{toor_id}')
+def toor_str(book_id: str):
+    return {'{toor_id_str}': book_id}
